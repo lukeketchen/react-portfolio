2f486887bb35f79208e57e5769c3510f4a2c0e0a
Luke Ketchen <luke.ketchen@gmail.com>
2020-01-29 15:57:12 +1100

    Sidebar menu
    + add links to socials
    + sizing of the sidebar
    
    ~ sizes starting to look good. Still not 100% happy with it but getting there

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,27 @@ const Header = () => (
     <h1>Luke Ketchen</h1>
     <img src={profilePic} alt="Profile Pic" />
     <h2>React Developer</h2>
+    <h3>
+      <i class="fas fa-location-arrow"></i> <br />
+      Hoppers Crossing, Vic
+    </h3>
+    <div className="icondiv">
+      <a href="https://github.com/lukeketchen" target="_blank">
+        <i class="fab fa-github"></i>
+      </a>
+      <a
+        href="https://www.instagram.com/luke.ketchen.photography"
+        target="_blank"
+      >
+        <i class="fab fa-instagram"></i>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/luke-ketchen-706882107"
+        target="_blank"
+      >
+        <i class="fab fa-linkedin"></i>
+      </a>
+    </div>
     <nav>
       <NavLink exact to="/" activeClassName="is-active">
         Home
